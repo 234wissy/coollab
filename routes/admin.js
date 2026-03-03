7f00bf43535719d64c6eb75b2ce26512fe2e5722
234wissy <119856140+234wissy@users.noreply.github.com>
2026-03-03 17:27:38 +0100

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ function adminOnly(req, res, next) {
 
 // ✅ Admin landing (requires admin)
 // GET /admin
-router.get("/admin/login", adminOnly, (req, res) => {
+router.get("/attendance/record_dashboard", adminOnly, (req, res) => {
   // Send admin to your real attendance dashboard
   return res.redirect("/attendance/record_dashboard");
 });
