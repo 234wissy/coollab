46dcb854b18ed11e61781b0e60e7ea6ce5f40a6c
234wissy <119856140+234wissy@users.noreply.github.com>
2026-03-03 21:24:52 +0100

    Update adminOnly.js

--- middleware/adminOnly.js
+++ middleware/adminOnly.js
@@ -1,4 +1,6 @@
 module.exports = function adminOnly(req, res, next) {
-  if (req.session && req.session.isAdmin) return next();
+  if (req.session?.isAdmin) return next();
+
+  // if someone hits admin pages without login, send them to admin login
   return res.redirect("/admin/login");
 };
